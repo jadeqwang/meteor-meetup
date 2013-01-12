2b37f9e95e826fad3eb2cc6aff549bf0734d7d62
David Garvey <david.1.bfg@gmail.com>
2013-01-11 18:46:45 -0800

    moved code into the devhouse files.
    Suspected problems with running on ubuntu.

--- devhouse.js
+++ devhouse.js
@@ -1,19 +1,47 @@
+Developer = new Meteor.Collection("developer");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to devhouse.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Template.welcome.events = {
+		'click button.buttonSubmit' : function() {
+			alert("1")
+			var re = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+			var email = $('#email')
+			if (!re.test(email.val())){
+				email.effect("shake", { times:3 }, 300);
+			}
+		}
+	};
+	
+//	Template.entryfield.events = {
+//			  "keydown #message": function(event){
+//			    if(event.which == 13){
+//			      // Submit the form
+//			      var name = document.getElementById('name');
+//			      var message = document.getElementById('message');
+//
+//			      if(name.value != '' && message.value != ''){
+//			        Messages.insert({
+//			          name: name.value,
+//			          message: message.value,
+//			          time: Date.now()
+//			        });
+//
+//			        name.value = '';
+//			        message.value = '';
+//			      }
+//			    }
+//			  }
+//			}
+	
+//  Template.hello.greeting = function () {
+//    return "Welcome to devhouse.";
+//  };
+//
+//  Template.hello.events({
+//    'click input' : function () {
+//      // template data, if any, is available in 'this'
+//      if (typeof console !== 'undefined')
+//        console.log("You pressed the button");
+//    }
+//  });
 }
